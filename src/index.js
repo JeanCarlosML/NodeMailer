19accfc580591b03d9cafff08460e8ea3a058717
Jeancarlos Mamani Lingan <jeancarlosmamanilingan@gmail.com>
2021-02-05 21:37:23 -0500

    mejorando servicio email

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const { enviarEmail } = require("./nodemailer/nodemailer");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get("/email", async (req, res) => {
+  try {
+    let { nombres , email, telefono, mensaje, asunto } = req.body;
+    let dataMail = { nombres, email, telefono, mensaje, asunto };
+    let data = await enviarEmail(dataMail);
+    res.status(200).json({
+      ok: true,
+      message: "Todo correcto",
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      error,
+      message: "Error interno",
+    });
+  }
+});
 
 app.listen(process.env.PORT || 3000, (e) => {
-  if (e) throw new Error(e);
+  if (e) {
+    console.log("Error al iniciar servidor");
+  } else {
+    console.log("Servidor iniciado con exito");
+  }
 });
--- src/nodemailer/nodemailer.js
+++ src/nodemailer/nodemailer.js
@@ -1,29 +1,48 @@
 const nodemailer = require("nodemailer");
 
-//Creando trasnporter
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
-//Configuraciones para nodemailer
-let mailOptions = {
-  from: process.env.EMAIL,
-  to: "akanzaakamaru@gmail.com",
-  subject: "Testing and testing",
-  //Se puede neviar texto plano text:"dsadas"
-  //Pero tambien se puede enviar html
-  //Se recomienda usar estilos en linea
-  html: `
+//Enviando email
+async function enviarEmail(data) {
+  //Creando trasnporter
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  //Configuraciones para nodemailer
+  let mailOptions = {
+    from: process.env.EMAIL,
+    //correo corporativo
+    to: "akanzaakamaru@gmail.com",
+    subject: "Solicitud proveniente de Página Web",
+    //Se puede neviar texto plano text:"dsadas"
+    //Pero tambien se puede enviar html
+    //Se recomienda usar estilos en linea
+    html: `
   <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
   <tr height="200px">  
       <td bgcolor="" width="600px">
-          <h1 style="color: #fff; text-align:center">Bienvenido</h1>
-          <p  style="color: #fff; text-align:center">
-              <span style="color: #e84393">Nombre</span> 
-              a la aplicación
+          <h1 style="color: #fff; text-align:center">Me gustaria contactar contigo</h1>
+          <p  style="color: #fff; text-align:start ; padding:10px 40px">
+              <span style="color: #e84393">Nombres: </span> 
+              ${data.nombres}
+          </p>
+          <p  style="color: #fff; text-align:start ;padding:10px 40px">
+              <span style="color: #e84393">Email: </span> 
+              ${data.email}
+          </p>
+          <p  style="color: #fff; text-align:start ; padding:10px 40px">
+              <span style="color: #e84393">Teléfono: </span> 
+              ${data.telefono}
+          </p>
+          <p  style="color: #fff; text-align:start ; padding:10px 40px">
+              <span style="color: #e84393">Mensaje: </span> 
+              ${data.mensaje}
+          </p>
+          <p  style="color: #fff; text-align:start ; padding:10px 40px">
+              <span style="color: #e84393">Asunto: </span> 
+              ${data.asunto}
           </p>
       </td>
   </tr>
@@ -34,13 +53,13 @@ let mailOptions = {
   </tr>
   </table>
 `,
-};
+  };
+  let respuesta = await transporter.sendMail(mailOptions).catch((e) => {
+    throw new Error("Error : " + e);
+  });
+  return respuesta;
+}
 
-//Enviando email
-transporter.sendMail(mailOptions, (e, data) => {
-  if (e) {
-    console.log("Error en send email");
-  } else {
-    console.log("Todo ok");
-  }
-});
+module.exports = {
+  enviarEmail,
+};
